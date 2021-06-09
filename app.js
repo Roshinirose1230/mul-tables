5272132b48b0066d3f6d14021bc244163bde1cf6
Roshinirose1230 <67871713+Roshinirose1230@users.noreply.github.com>
2021-06-09 13:07:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+
+
+
+
+const a = parseInt(1);
+    for(let i = 1; i <= 10; i++) {
+    
+        
+        const result = i * a;
+        console.log('1*' + i +'=' + result);}
+    
+        const b = parseInt(2);
+
+        for(let j = 1; j <= 10; j++) {
+        
+            
+            const result = j * b;
+            console.log('2*' + j +'=' + result);}
+            const c = parseInt(3);
+
+            for(let k = 1; k <= 10; k++) {
+            
+                
+                const result = k * c;
+                console.log('3*' + k +'=' + result);}
+
+                const d = parseInt(4);
+
+                for(let l = 1; l <= 10; l++) {
+                
+                    
+                    const result = l * d;
+                    console.log('4*' + l +'=' + result);}
+                    const e = parseInt(5);
+
+                    for(let m = 1; m <= 10; m++) {
+                    
+                        
+                        const result = m * e;
+                        console.log('5*' + m +'=' + result);}
+                        const f = parseInt(6);
+
+                        for(let n = 1; n <= 10; n++) {
+                        
+                            
+                            const result = n * f;
+                            console.log('6*' + n +'=' + result);}
+                            const g = parseInt(7);
+
+                            for(let o = 1; o <= 10; o++) {
+                            
+                                
+                                const result = o * g;
+                                console.log('7*' + o +'=' + result);}
+                                const h = parseInt(8);
+
+                                for(let p = 1; p <= 10; p++) {
+                                
+                                    
+                                    const result = p * h;
+                                    console.log('8*' + p +'=' + result);}
+                                    const i = parseInt(9);
+
+                                    for(let q = 1; q <= 10; q++) {
+                                    
+                                        
+                                        const result = q * i;
+                                        console.log('9*' + q +'=' + result);}
+                                        const z = parseInt(10);
+
+                                        for(let r = 1 ; r <= 10; r++) {
+                                        
+                                            
+                                            const result = r * z;
+                                            console.log('10*' + r +'=' + result);}
+                                                                            
